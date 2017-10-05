639bd1d3c8d54bed004ee12e0ce536b2552f3ddd
dames <dames@pageuppeople.com>
2017-10-05 16:00:19 +1100

    Added agent command

--- maintenance/statistics/Rebuild Statistics For All User Databases at 2am Saturday morning.sql
+++ maintenance/statistics/Rebuild Statistics For All User Databases at 2am Saturday morning.sql
@@ -6,13 +6,85 @@
 
 	It is dependent on /lib/PUP-Ola Hallengren Index Management.sql
 
-*/
+*/USE [msdb]
+GO
 
+/****** Object:  Job [Rebuild Statistics - All User databases]    Script Date: 10/5/2017 3:50:58 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 10/5/2017 3:50:58 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+IF EXISTS (SELECT * FROM  msdb.dbo.sysjobs WHERE name = 'Rebuild Statistics - All User databases')
+BEGIN
+	PRINT 'Already installed, skipping'
+	GOTO EndSave
+END 
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'Rebuild Statistics - All User databases', 
+		@enabled=1, 
+		@notify_level_eventlog=2, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'AMESLAPTOP\david', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Rebuild Statistics]    Script Date: 10/5/2017 3:50:58 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Rebuild Statistics', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'
 EXECUTE [dbo].[IndexOptimize]
-    @Databases = 'USER_DATABASES' ,
+    @Databases = ''USER_DATABASES'' ,
     @FragmentationLow = NULL ,
     @FragmentationMedium = NULL ,
     @FragmentationHigh = NULL ,
-    @UpdateStatistics = 'ALL' ,
-    @OnlyModifiedStatistics = N'Y' ,
-    @LogToTable = N'Y';
+    @UpdateStatistics = ''ALL'' ,
+    @OnlyModifiedStatistics = N''Y'' ,
+    @LogToTable = N''Y'';', 
+		@database_name=N'master', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'Rebuild Statistics - 2am Saturday', 
+		@enabled=1, 
+		@freq_type=8, 
+		@freq_interval=64, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20171005, 
+		@active_end_date=99991231, 
+		@active_start_time=20000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'79e53ae6-b590-4aea-952b-8d0cf1aa8335'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+
+GO
+
